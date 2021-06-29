28f73e0182a1b0f4a3f370bb7cb6229c785ea720
themrusgame <45094736+themrusgame@users.noreply.github.com>
2021-06-29 19:57:51 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+  $(".reviews__item-rating").rateYo({
+    starWidth: "16px",
+    ratedFill: "#FABB2D",
+    spacing: "2px",
+    readOnly: true,
+    starSvg: '<svg width="16" height="15" viewBox="0 0 16 15" fill="none"><path d = "M7.9375 0.6875L9.99218 5.56813L15.011 6.12899L11.262 9.70625L12.3092 14.9335L7.9375 12.2638L3.56585 14.9335L4.61296 9.70625L0.864017 6.12899L5.88282 5.56813L7.9375 0.6875Z" fill = "#FABB2D"/></svg>'
+    
+  });
+
+});
